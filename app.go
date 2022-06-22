cccee1209a7056b6d7395fcbc944839048d1921f
zhanknight <zhanknight@gmail.com>
2022-06-22 17:47:39 -0400

    Clean up variable declarations

--- app.go
+++ app.go
@@ -5,18 +5,23 @@ import (
 	"math/rand"
 	"strings"
 	"sync"
-	"time"
 )
 
 // This is a starry night ascii generator.
-const words string = "~ ~ ~ * ~ ~ * ~ ~ / * / * O ~ ~ ~ ~"
-const numLines int = 8
+
+const (
+	words    string = "~ ~ ~ * ~ ~ * ~ ~ / * / * O ~ ~ ~ ~"
+	numLines int    = 8
+)
+
+var (
+	wg           sync.WaitGroup
+	mutex        sync.Mutex
+	finalString  string
+	stringPieces []string
+)
 
 func main() {
-	var wg sync.WaitGroup
-	var mutex sync.Mutex
-	var finalString string
-	var stringPieces []string
 	for i := 0; i < numLines; i++ {
 		wg.Add(1)
 		go func() {
@@ -40,9 +45,9 @@ func lineGenerator() string {
 	rand.Shuffle(len(shuffledWords), func(i, j int) {
 		shuffledWords[i], shuffledWords[j] = shuffledWords[j], shuffledWords[i]
 	})
-	var output = strings.Join(shuffledWords, " ")
+	output := strings.Join(shuffledWords, " ")
 	// adding a delay to simulate a task that takes time
-	time.Sleep(100 * time.Millisecond)
+	// time.Sleep(100 * time.Millisecond)
 	return output
 }
 
